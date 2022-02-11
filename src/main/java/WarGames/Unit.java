af045d4c412cafd39e966b20390a6f3e4e22ef5b
Charlotte Christiansen <Charlohc@ntnu.no>
2022-02-11 15:01:50 +0100

    Second commit - Unit complete

--- src/main/java/WarGames/Unit.java
+++ src/main/java/WarGames/Unit.java
@@ -1,4 +1,47 @@
 package WarGames;
 
-public class Unit {
+//TODO: Java Doc
+public abstract class Unit{
+    private final String name;
+    private int health;
+    private final int attack;
+    private final int armor;
+
+//TODO: Implement checks
+    public Unit(String name, int health, int attack, int armor){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public abstract int getAttackBonus();
+
+    public abstract int getResistBonus();
+
+    public void attack() {
+        int healthOpponent = getAttack() - (getAttack() + getAttackBonus()) + (getArmor() + getResistBonus());
+    }
+
+
 }
